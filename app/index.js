5e5c08a05fa0d65ae2adb412e519f4099353c865
Xing <xingmarc@yeah.net>
2016-09-22 19:38:49 -0700

    First Commit

---
+++ app/index.js
@@ -0,0 +1,77 @@
+var USER_DATA = {
+	name: 'yichi Xing',
+	username: 'xingmarc',
+	image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+};
+
+var React = require ('react');
+var ReactDOM = require('react-dom');
+
+var routes = require('./config/routes');
+
+/*
+	All React components should be:
+	(FIRST)
+	Focused
+	Independent
+	Reusable
+	Small
+	Testable
+*/
+// var Link = React.createClass({
+// 	changeUrl: function() {
+// 		window.location.replace(this.props.href);
+// 	},
+// 	render: function() {
+// 		return (
+// 			<span style={{color: 'blue', cursor: 'pointer'}}
+// 			onClick={this.changeUrl}>
+// 				{this.props.children}
+// 			</span>
+// 		)
+// 	}
+// })
+
+// var ProfilePic = React.createClass({
+// 	render: function() {
+// 		return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+// 	}
+// });
+
+// var ProfileLink = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div>
+// 				<Link href={'https://github.com/' + this.props.username}>
+// 					{this.props.username}
+// 				</Link>
+// 			</div>
+// 		)
+// 	}
+// });
+
+// var ProfileName = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div>{this.props.name}</div>
+// 		)
+// 	}
+// });
+
+// var Avatar = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div>
+// 				<ProfilePic imageUrl={this.props.user.image} />
+// 				<ProfileName name={this.props.user.name} />
+// 				<ProfileLink username={this.props.user.username} />
+// 			</div>
+// 		)
+// 	}
+// })
+
+
+ReactDOM.render(
+	routes,
+	document.getElementById('app')
+)
